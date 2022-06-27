0e00d1744588e903b3fdd559edabfe073be844cf
matt70iu <mattadams60@gmail.com>
2022-06-27 15:12:04 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,10 +4,20 @@
  */
 
 // Vanilla JavaScript Function
+//function addTwoNumbers(a, b) {
+//   return a + b;
+
+
+//let sum = addTwoNumbers(3, 5);
+//console.log(sum)
 
 // Arrow Function With Parameters
 
+
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2)
 
 // Implicit Returns
 
